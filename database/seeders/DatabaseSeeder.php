858c96a6a5c4dff35116ac46b4efbdf191701818
Judithe Taonsa <judith.taonsa>
2021-04-02 11:15:48 +0100

    commit2 du 02042021

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
-        //\App\Models\Produit::factory(100)->create();
-       // \App\Models\Ministere::factory(100)->create();
+        \App\Models\Produit::factory(100)->create();
+        \App\Models\Ministere::factory(100)->create();
     }
 }
